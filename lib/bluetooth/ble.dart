1e3252a749b7dbabce39fc148e6682d2e8ee49cc
hellozed <64930198+hellozed@users.noreply.github.com>
2020-09-02 23:12:37 -0700

    BLE working for PPG stream

--- lib/bluetooth/ble.dart
+++ lib/bluetooth/ble.dart
@@ -1,28 +1,22 @@
-//import 'dart:ffi';
-
 import 'package:flutter_blue/flutter_blue.dart';
 import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'dart:convert'; //utf8.encode
+//import 'dart:io'; //stdout.write();
 
 import 'dart:typed_data'; //for data formatting
 import '../google_chart/live_line_chart.dart';
 /*
   FIXME:
-  1. ble device could not be found when hot load again the app
-  current fix: press the "reset" of the device, then "hot load" the appl.
-  electrocardiography
-  
-  2. Stream<List<int>> ecgStream; 
-  
-  3. characteristicUuid: 00002a6e-0000-1000-8000-00805f9b34fb 
-  serviceUuid: 00001809-0000-1000-8000-00805f9b34fb
-  remoteId: 6DF7B035-9A74-F291-67A9-CF8113AA482E 
-  how to change remoteId
+  Stream<List<int>> ecgStream; 
 */
 
 /* ----------------------------------------------------------------------------
  * Credit:   
  * 
  * The code here is learned from the link below.
+ * Based on Neil Kolban example: 
+ * https://github.com/nkolban/ESP32_BLE_Arduino
  * 
  * https://medium.com/flutter-community/flutter-for-bluetooth-devices-5594f105b146
  * Author's site: https://github.com/MDSADABWASIM  
@@ -44,42 +38,48 @@ import '../google_chart/live_line_chart.dart';
  * UUID Define  
  * Alert: These definition value must be same as "ble.dart" in the Flutter project 
  * name below Ser = Service, Chr = Characteristic 
+ * 
+ * example uuid
+ * characteristicUuid: 00002a6e-0000-1000-8000-00805f9b34fb 
+          serviceUuid: 00001809-0000-1000-8000-00805f9b34fb
+             remoteId: 6DF7B035-9A74-F291-67A9-CF8113AA482E 
  * ----------------------------------------------------------------------------*/
-var deviceName          =  'HomeICU';  
+var deviceName = 'HomeICU';
 
-var endUUID             = '-0000-1000-8000-00805f9b34fb';
+var endUUID = '-0000-1000-8000-00805f9b34fb';
 
-var heartRateSerUUID    = Guid('0000180D'+endUUID);
-var heartRateChrUUID    = Guid('00002A37'+endUUID);
+var heartRateSerUUID = Guid('0000180D' + endUUID);
+var heartRateChrUUID = Guid('00002A37' + endUUID);
 
-var sPO2SerUUID         = Guid('00001822'+endUUID);
-var sPO2ChrUUID         = Guid('00002A5E'+endUUID);
+var sPO2SerUUID = Guid('00001822' + endUUID);
+var sPO2ChrUUID = Guid('00002A5E' + endUUID);
 
-var dataStreamSerUUID   = Guid('00001122'+endUUID);
-var ecgStreamChrUUID    = Guid('00001424'+endUUID);
-var ppgStreamChrUUID    = Guid('00001425'+endUUID);
+var dataStreamSerUUID = Guid('00001122' + endUUID);
+var ecgStreamChrUUID = Guid('00001424' + endUUID);
+var ppgStreamChrUUID = Guid('00001425' + endUUID);
 
-var tempSerUUID         = Guid('00001809'+endUUID);
-var tempChrUUID         = Guid('00002a6e'+endUUID);
+var tempSerUUID = Guid('00001809' + endUUID);
+var tempChrUUID = Guid('00002a6e' + endUUID);
 
-var batterySerUUID      = Guid('0000180F'+endUUID);
-var batteryChrUUID      = Guid('00002a19'+endUUID);
+var batterySerUUID = Guid('0000180F' + endUUID);
+var batteryChrUUID = Guid('00002a19' + endUUID);
 
-var hrvSerUUID          = Guid("cd5c7491-4448-7db8-ae4c-d1da8cba36d0");
-var hrvChrUUID          = Guid("01bfa86f-970f-8d96-d44d-9023c47faddc");
-var histChrUUID         = Guid("01bf1525-970f-8d96-d44d-9023c47faddc");
+var hrvSerUUID = Guid("cd5c7491-4448-7db8-ae4c-d1da8cba36d0");
+var hrvChrUUID = Guid("01bfa86f-970f-8d96-d44d-9023c47faddc");
+var histChrUUID = Guid("01bf1525-970f-8d96-d44d-9023c47faddc");
 
-FlutterBlue             flutterBlue;
-BluetoothDevice         bleDevice;            // esp32 board device
+FlutterBlue flutterBlue; // mobile app
+BluetoothDevice bleDevice; // esp32 board device
 
-int batteryPercent;
-int heartRate;
-int sPO2Percent;
+var heartRate2 = new List<int>();
+var battery2 = new List<int>();
+var spo2 = new List<int>();
+var temp2 = new List<int>();
+var hist2 = new List<int>();
+var hrv2 = new List<int>();
+var ecg2 = new List<int>();
+var ppg2 = new List<int>();
 
-int ecgStream;
-double bodyTemperature;
-int heartRateVariability;
-int respirationRate;
 /* ----------------------------------------------------------------------------
  *
  * Phase 1. initialisation and listen to device state
@@ -88,50 +88,73 @@ int respirationRate;
 void bleInitState() {
   flutterBlue = FlutterBlue.instance;
   flutterBlue.state.listen((state) {
-    if (state == BluetoothState.off){
-      print("ble power OFF, you must turn it on");  //Alert user to turn on bluetooth.
+    if (state == BluetoothState.off) {
+      print(
+          "ble: power OFF, you must turn it on"); // notice user to turn on bluetooth.
     } else if (state == BluetoothState.on) {
-      print("ble power ON");
-      scanForDevices(); 
+      print("ble: power ON");
+      scanForDevices();
     }
   });
 }
 
 // Scan and Stop Bluetooth
-void scanForDevices() async {  
-  StreamSubscription<ScanResult>   scanSubscription;
+void scanForDevices() async {
+  StreamSubscription<ScanResult> scanSubscription;
 
-  scanSubscription = flutterBlue.scan(withServices:[batterySerUUID], timeout: Duration(seconds: 5))
-  .listen((scanResult) async {
+  List<BluetoothDevice> devices = await FlutterBlue.instance.connectedDevices;
+  if (devices.length > 0) {
+    if (devices[0].name == deviceName) {
+      print("ble: device already connected! disconnecting now ...");
+      await devices[0].disconnect();
+    }
+  }
+
+  scanSubscription = flutterBlue.scan(
+      withServices: [batterySerUUID],
+      timeout: Duration(seconds: 5)).listen((scanResult) async {
     if (scanResult.device.name == deviceName) {
-      await flutterBlue.stopScan();         // stop scan
+      await flutterBlue.stopScan(); // stop scan
       await scanSubscription.cancel();
 
-      print("found device");
-      bleDevice = scanResult.device;        // assign bluetooth device
+      print("ble: found device");
+
+      bleDevice = scanResult.device; // assign bluetooth device
+
       await bleConnectToDevice();
-      
-      bleDevice.state.listen((event) async{ 
-        if (event==BluetoothDeviceState.disconnected){
-            print("Ble disconnect now!");
-            bleConnectToDevice();           // re-connect device
-        }
-        else 
-           print(event);   
+
+      bleDevice.state.listen((event) async {
+        if (event == BluetoothDeviceState.disconnected) {
+          print("ble: device disconnected");
+          bleConnectToDevice(); // re-connect device
+        } else
+          print(event);
       });
     }
   });
 }
+
 /* ----------------------------------------------------------------------------
  *
  * Phanse 2. connect to device
  * 
  * ----------------------------------------------------------------------------*/
+var characteristicsNumber = 0;
+BluetoothCharacteristic chrPPG;
 Future bleConnectToDevice() async {
   await bleDevice.connect();
 
+  print("ble: clear cache");
+  battery2.clear();
+  heartRate2.clear();
+  spo2.clear();
+  temp2.clear();
+  hrv2.clear();
+  hist2.clear();
+  ecg2.clear();
+  ppg2.clear();
+
   // discover, connect, and listen the characteristics
-//FIXME
 
   await bleDiscoverServices(
       "Battery", batterySerUUID, batteryChrUUID, batteryDataHandler);
@@ -148,80 +171,150 @@ Future bleConnectToDevice() async {
       "EcgData", dataStreamSerUUID, ecgStreamChrUUID, ecgStreamDataHandler);
   await bleDiscoverServices(
       "PpgData", dataStreamSerUUID, ppgStreamChrUUID, ppgStreamDataHandler);
+
+  while (true) {
+    await Future.delayed(Duration(seconds: 1));
+    if (characteristicsNumber == 8) {
+      List<int> data = utf8.encode("OK"); 
+      print("ble: found all characteristics");
+      chrPPG.write(data);
+      break;
+    }
+  }
 }
 
 Future bleDiscoverServices(String msg, Guid serviceUuid,
     Guid characteristicUuid, void Function(List<int>) dataProcessing) async {
-  BluetoothCharacteristic result;
   List<BluetoothService> services = await bleDevice.discoverServices();
+  BluetoothCharacteristic resultCharacteristic;
   services.forEach((service) {
+    //locate service id
     if (service.uuid == serviceUuid) {
       service.characteristics.forEach((characteristic) {
+        //locate characteristic id
         if (characteristic.uuid == characteristicUuid) {
           print("ble $msg connected");
-          result = characteristic;
-          result.setNotifyValue(true);
-          result.value.listen(dataProcessing);
+          resultCharacteristic = characteristic;
+          resultCharacteristic.setNotifyValue(true);
+          resultCharacteristic.value.listen(dataProcessing);
+          characteristicsNumber++;
+
+          if (characteristic.uuid == ppgStreamChrUUID) chrPPG = characteristic;
         }
       });
     }
   });
-  if (result == null) print("$msg: serverice/characteristic not Found!");
+  if (resultCharacteristic == null) print("ble: error! $msg: not Found.");
 }
-
 /* ----------------------------------------------------------------------------
  *
  * callback functions for data processing
  * 
  * ----------------------------------------------------------------------------*/
+
+bool dataValid(List<int> data, List<int> previousData, String printInfo) {
+  if (data.length == 0) return false; //received zero data
+
+  bool isEqual = listEquals<int>(data, previousData);
+  if (isEqual) return false; //receive same data again, ignore it
+
+  previousData.clear();
+  previousData.addAll(data);
+
+  print(printInfo + ": $data");
+  return true;
+}
+
 void batteryDataHandler(List<int> data) {
-  // print(data);
+  if (dataValid(data, battery2, "battery")) {
+    // process data here here
+  }
 }
 
 void heartRateDataHandler(List<int> data) {
-  // print(data);
+  if (dataValid(data, heartRate2, "heart rate")) {
+    // process data here here
+  }
 }
 
 void sPO2chrDataHandler(List<int> data) {
-  // print(data);
-}
-
-void ecgStreamDataHandler(List<int> data) {
-//  print(data);
-}
-
-void ppgStreamDataHandler(List<int> data) {
-  print(data);
-  data.forEach((element) {
-    // remove the first data point on the left
-    liveChartData.removeAt(0);
-
-    // each x decrese by 1 to shift the chart left
-    liveChartData.forEach((element) {
-      element.x--;
-    });
-
-    // add one time at the end of the right side
-    liveChartData.add(ChartData(liveChartData.length, element));
-  });
+  if (dataValid(data, spo2, "spo2")) {
+    // process data here here
+  }
 }
 
 void temperatureDataHandler(List<int> data) {
-  //convert fout-byte list into double float
-  ByteBuffer buffer = new Int8List.fromList(data).buffer;
-  ByteData byteData = new ByteData.view(buffer);
-  bodyTemperature = byteData.getFloat32(0, Endian.little);
+  double bodyTemperature;
 
-  //print("Tempe:, ${bodyTemperature.toStringAsFixed(2)}");  //bodyTemperature  = data;
+  // esp32 float is 32 bits, app float is 64-bit double
+
+  if (dataValid(data, temp2, "temperature")) {
+    //convert fout-byte list into double float
+    ByteBuffer buffer = new Int8List.fromList(data).buffer;
+    ByteData byteData = new ByteData.view(buffer);
+    bodyTemperature = byteData.getFloat32(0, Endian.little);
+    //print("body temperature = : ${bodyTemperature.toStringAsFixed(3)}");
+  }
 }
 
 void hrvDataHandler(List<int> data) {
-  // print(data);
-}
-void histDataHandler(List<int> data) {
-  // print(data);
+  if (dataValid(data, hrv2, "hrv")) {
+    // process data here here
+  }
 }
 
+void histDataHandler(List<int> data) {
+  if (dataValid(data, hist2, "hist")) {
+    // process data here here
+  }
+}
+
+void updateGraph(List<int> data, List<int> data2, int len, String printInfo,
+    List<ChartData> chartData) {
+  if (data.length == 0) return; //received zero data
+
+  if (data.length < len) {
+    //data len = pack size + 1 serial number
+    print("len=${data.length}, byte missing");
+    return;
+  }
+
+  bool isEqual = listEquals<int>(data, data2);
+  if (isEqual) return; //receive same data again, ignore it
+
+  data2.clear();
+  data2.addAll(data);
+
+  //remove the last two bytes of serial number, and convert to int16
+  var data8 = new Uint8List.fromList(data);
+  var data16 = new List.from(data8.buffer.asInt16List(), growable: true);
+  data16.removeLast();
+  //print("ppg16+: $data16"); //with serial number
+
+  data16.forEach((element) {
+    // remove the first data point on the left
+    chartData.removeAt(0);
+
+    // each x decrese by 1 to shift the chart left
+    chartData.forEach((element2) {
+      element2.x--;
+    });
+    // add one time at the end of the right side
+    chartData.add(ChartData(liveChartData.length, element));
+  });
+  return;
+}
+
+//these must be same as firmware project
+const ecg_tx_size = 10; //
+const ppg_tx_size = 10; //
+
+void ecgStreamDataHandler(List<int> data) {
+  //updateGraph(data, ecg2, ecg_tx_size,"ecg", liveChartData);
+}
+void ppgStreamDataHandler(List<int> data) {
+  updateGraph(data, ppg2, ppg_tx_size * 2 + 2, "ppg", liveChartData);
+}
 /* ----------------------------------------------------------------------------
  * Phase 3.
  * 
@@ -257,16 +350,7 @@ void interpretReceivedData(String data) async {
  * 
  * Send commands to the device
  * ----------------------------------------------------------------------------*/
-void sendTransparentData(String dataString) async {
-  
-  List<int> data = utf8.encode(dataString);     //Encoding the string
-  
-  if (bleDeviceState == BluetoothDeviceState.connected){
-  await chr.write(data);
-//await chr.write(_getRandomBytes(), withoutResponse: true);
-//await chr.write([0x12, 0x34])
-  }
-}
+
 
 In Async Code
 await Future.delayed(Duration(seconds: 1));
